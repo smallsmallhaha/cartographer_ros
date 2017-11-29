73658a3b39b887c1758f20b0ddada31d1b071c03
kekliu <kekliu.priv@gmail.com>
2017-11-29 22:29:55 +0800

    添加配置文件 backpack_2d.lua 的注释

--- cartographer_ros/configuration_files/backpack_2d.lua
+++ cartographer_ros/configuration_files/backpack_2d.lua
@@ -23,17 +23,33 @@ options = {
   published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = true,
+  -- 是否使用[Odometry]消息,为位姿外推器提供位置和姿态
   use_odometry = false,
+  -- 要使用的[LaserScan]的个数
   num_laser_scans = 0,
+  -- 要使用的[MultiEchoLaserScan]的个数
   num_multi_echo_laser_scans = 1,
+  -- 将每个[LaserScan/MultiEchoLaserScan]平均分成十份,封装成RangeData送入上层
+  -- TRAJECTORY_BUILDER_2D.scans_per_accumulation 表示将每scans_per_accumulation个
+  --       RangeData合成一个scan,做scan-to-map匹配和submap的构造
+  -- 上层对RangeData的处理过程详见cartographer中配置文件的注释
+  -- 建议该值不要太大
   num_subdivisions_per_laser_scan = 10,
+  -- 要使用的[PointCloud2]的个数
   num_point_clouds = 0,
+  -- 获取坐标系转换信息的延迟
   lookup_transform_timeout_sec = 0.2,
+  -- 发布子图列表[SubmapList]消息的频率(不需要太快)
   submap_publish_period_sec = 0.3,
+  -- 发布已匹配点云[PointCloud2]/更新外推器位姿/发布坐标变换消息的频率
   pose_publish_period_sec = 5e-3,
+  -- 发布路径和路标[MarkerArray]的频率
   trajectory_publish_period_sec = 30e-3,
+  -- rangefinder消息采样比率,为1表示采样所有数据
   rangefinder_sampling_ratio = 1.,
+  -- Odometry消息采样比率
   odometry_sampling_ratio = 1.,
+  -- Imu消息采样比率
   imu_sampling_ratio = 1.,
 }
 
